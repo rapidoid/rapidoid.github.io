6a964e1ceca6d3620b10ca192e77c26f538fadd6
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2015-02-24 18:08:48 +0100

    Added new examples and other minor improvements.

--- examples/rapidoid.js
+++ examples/rapidoid.js
@@ -5,8 +5,7 @@ try {
 } catch (e) {
 }
 if (theme && theme != 'default') {
-	document.write('<link href="../theme-' + theme
-			+ '.css" rel="stylesheet">');
+	document.write('<link href="../theme-' + theme + '.css" rel="stylesheet">');
 }
 
 function goAt(url) {
@@ -23,73 +22,8 @@ function _stop(ev) {
 	}
 }
 
-
 function _emit(ev, eventId) {
-
-	_stop(ev);
-
-	var x = document.querySelectorAll("input,textarea");
-	var inputs = {};
-	for (var i = 0; i < x.length; i++) {
-		var t = $(x[i]);
-		var _h = t.attr('_h');
-
-		if (_h) {
-			var val;
-
-			if (t.prop('type') == 'checkbox' || t.prop('type') == 'radio') {
-				val = t.prop('checked');
-			} else {
-				val = t.val();
-			}
-
-			inputs[_h] = val;
-		}
-	}
-
-	x = document.querySelectorAll("option");
-
-	for (var i = 0; i < x.length; i++) {
-		var t = $(x[i]);
-		var _h = t.attr('_h');
-
-		if (_h) {
-			inputs[_h] = t.prop('selected');
-		}
-	}
-
-	$.post(window.location.href, {
-		event : eventId,
-		inputs : JSON.stringify(inputs)
-	}).done(function(data) {
-		if (data._redirect_) {
-			goAt(data._redirect_);
-			return;
-		}
-		for ( var sel in data) {
-			if (sel == "!errors") {
-				$('.field-error').html('');
-				errors = data[sel];
-				for ( var h in errors) {
-					var err = errors[h];
-
-					var x = document.querySelectorAll("input,textarea,option");
-					for (var i = 0; i < x.length; i++) {
-						var t = $(x[i]);
-						var _h = t.attr('_h');
-						if (_h == h) {
-							$(t).next('.field-error').html(err);
-						}
-					}
-				}
-			} else {
-				$(sel).html(data[sel]);
-			}
-		}
-	}).fail(function(data) {
-		alert("Error!");
-		console.log(data);
-	});
+	alert("This is not a live demo, so this button doesn't work!");
 }
 
 function _popup(popupUrl, onClosed) {
