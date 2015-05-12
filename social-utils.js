86dd6454ebcac104a6a07d9f29206b2695b0094c
nikolce.mihajlovski <nikolce.mihajlovski@gmail.com>
2015-05-12 13:21:46 +0200

    Fixed twitter button and moved scripts to bottom.

--- social-utils.js
+++ social-utils.js
@@ -14,19 +14,6 @@
 ga('create', 'UA-47578991-1', 'java8.org');
 ga('send', 'pageview');
 
-// TWITTER
-
-!function(d, s, id) {
-	var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http'
-			: 'https';
-	if (!d.getElementById(id)) {
-		js = d.createElement(s);
-		js.id = id;
-		js.src = p + '://platform.twitter.com/widgets.js';
-		fjs.parentNode.insertBefore(js, fjs);
-	}
-}(document, 'script', 'twitter-wjs');
-
 // MAILCHIMP
 
 var fnames = new Array();
